2657eecb94dece68b72c50aa40be4e217e829c22
brandnn07 <brandcarter4@gmail.com>
2021-09-17 21:15:53 -0400

    file now displays an image

--- js/index.js
+++ js/index.js
@@ -1,34 +1,71 @@
-const initApp = () => {
-    const droparea = document.querySelector('.droparea');
-
-    const active = () => droparea.classList.add("green-border");
-
-    const inactive = () => droparea.classList.remove("green-border");
-
-    const prevents = (e) => e.preventDefault();
-
-    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(evtName => {
-        droparea.addEventListener(evtName, prevents);
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+  
+    dropZoneElement.addEventListener("click", (e) => {
+      inputElement.click();
     });
-
-    ['dragenter', 'dragover'].forEach(evtName => {
-        droparea.addEventListener(evtName, active);
+  
+    inputElement.addEventListener("change", (e) => {
+      if (inputElement.files.length) {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+      }
     });
-
-    ['dragleave', 'drop'].forEach(evtName => {
-        droparea.addEventListener(evtName, inactive);
+  
+    dropZoneElement.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
     });
-
-    droparea.addEventListener("drop", handleDrop);
-
-}
-
-document.addEventListener("DOMContentLoaded", initApp);
-
-const handleDrop = (e) => {
-    const dt = e.dataTransfer;
-    const files = dt.files;
-    const fileArray = [...files];
-    console.log(files); // FileList
-    console.log(fileArray);
-}
+  
+    ["dragleave", "dragend"].forEach((type) => {
+      dropZoneElement.addEventListener(type, (e) => {
+        dropZoneElement.classList.remove("drop-zone--over");
+      });
+    });
+  
+    dropZoneElement.addEventListener("drop", (e) => {
+      e.preventDefault();
+  
+      if (e.dataTransfer.files.length) {
+        inputElement.files = e.dataTransfer.files;
+        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      }
+  
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+  
+  /**
+   * Updates the thumbnail on a drop zone element.
+   *
+   * @param {HTMLElement} dropZoneElement
+   * @param {File} file
+   */
+  function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+  
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+      dropZoneElement.querySelector(".drop-zone__prompt").remove();
+    }
+  
+    // First time - there is no thumbnail element, so lets create it
+    if (!thumbnailElement) {
+      thumbnailElement = document.createElement("div");
+      thumbnailElement.classList.add("drop-zone__thumb");
+      dropZoneElement.appendChild(thumbnailElement);
+    }
+  
+    thumbnailElement.dataset.label = file.name;
+  
+    // Show thumbnail for image files
+    if (file.type.startsWith("image/")) {
+      const reader = new FileReader();
+  
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+      };
+    } else {
+      thumbnailElement.style.backgroundImage = null;
+    }
+  }
